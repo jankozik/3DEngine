42ae5f06c13139969a0d7319cd52918ff19851dd
3DEngine <talented0220@gmail.com>
2016-10-10 16:32:02 -0400

    Fixed links to two helper classes

--- 3DEngine/Classes/Helpers/ArrayExtensions.vb
+++ 3DEngine/Classes/Helpers/ArrayExtensions.vb
@@ -1,1 +1,237 @@
-../../../../Countdown/Countdown/ArrayExtensions.vb
+﻿Imports System.Runtime.CompilerServices
+
+Module ArrayExtensions
+    <Extension()>
+    Public Sub Add(Of T)(ByRef n() As T, value As T)
+        ReDim Preserve n(n.Length)
+        n(n.Length - 1) = value
+    End Sub
+
+    <Extension()>
+    Public Sub Add(Of T)(ByRef n1() As T, n2() As T)
+        n1.Add(n2, 0, n2.Length)
+    End Sub
+
+    <Extension()>
+    Public Sub Add(Of T)(ByRef n1() As T, n2() As T, length As Integer)
+        n1.Add(n2, 0, length)
+    End Sub
+
+    <Extension()>
+    Public Sub Add(Of T)(ByRef n1() As T, n2() As T, offset As Integer, length As Integer)
+        ReDim Preserve n1(n1.Length + length - 1)
+        Array.Copy(n2, offset, n1, n1.Length - length, length)
+    End Sub
+
+    <Extension()>
+    Public Sub RemoveByValue(Of T)(ByRef n() As T, value As T)
+        n = RemoveByValue2(n, value)
+    End Sub
+
+    <Extension()>
+    Private Function RemoveByValue2(Of T)(n() As T, value As T) As T()
+        Dim r() As T = {}
+        For i As Integer = 0 To n.Length - 1
+            If Not n(i).Equals(value) Then r.Add(n(i))
+        Next
+
+        Return r
+    End Function
+
+    <Extension()>
+    Public Function RemoveByIndex(Of T)(n() As T, index As Integer) As T()
+        Dim r(n.Length - 2) As T
+        Dim j As Integer
+        For i As Integer = 0 To n.Length - 1
+            If i <> index Then
+                r(j) = n(i)
+                j += 1
+            End If
+        Next
+
+        Return r
+    End Function
+
+    <Extension()>
+    Public Function Permutate(Of T)(n() As T) As T()()
+        Dim subPerm()() As T
+        Dim length As Integer = n.Length
+        Dim res() As T = {}
+
+        If n.Length = 2 Then
+            res = New T() {n(0), n(1), n(1), n(0)}
+        Else
+            For i As Integer = 0 To length - 1
+                subPerm = Permutate(n.RemoveByIndex(i))
+                For j As Integer = 0 To subPerm.Length - 1
+                    res.Add(n(i))
+                    res.Add(subPerm(j))
+                Next
+            Next
+        End If
+
+        Dim k()() As T = {}
+        For i As Integer = 0 To res.Length \ length - 1
+            ReDim Preserve k(i)
+            k(i) = New T() {}
+            k(i).Add(res, i * length, length)
+        Next
+
+        Return k
+    End Function
+
+    <Extension()>
+    Public Function Permutate2(Of T)(n() As T, Optional deLinearize As Boolean = True) As T()()
+        Dim subPerm()() As T
+        Dim length As Integer = n.Length
+        Dim subPermNum As Integer = (length - 1).Fact()
+        Dim res() As T = {}
+
+        If n.Length = 2 Then
+            res = New T() {n(0), n(1), n(1), n(0)}
+        Else
+            For i As Integer = 0 To length - 1
+                subPerm = Permutate2(n.RemoveByIndex(i), False)
+                Dim permLen As Integer = subPerm(0).Length \ subPermNum
+                For j As Integer = 0 To subPermNum - 1
+                    res.Add(n(i))
+                    res.Add(subPerm(0), j * permLen, permLen)
+                Next
+            Next
+        End If
+
+        Dim k()() As T = {}
+        If deLinearize Then
+            For i As Integer = 0 To res.Length \ length - 1
+                ReDim Preserve k(i)
+                k(i) = New T() {}
+                k(i).Add(res, i * length, length)
+            Next
+        Else
+            ReDim k(0)
+            k(0) = New T() {}
+            k(0).Add(res)
+        End If
+
+        Return k
+    End Function
+
+    <Extension()>
+    Public Function Permutate3(Of T)(n() As T, Optional deLinearize As Boolean = True) As T()()
+        Dim subPerm()() As T
+        Dim length As Integer = n.Length
+        Dim permNum As Integer = length.Fact()
+        Dim res(permNum * length - 1) As T
+
+        If length = 2 Then
+            res = New T() {n(0), n(1), n(1), n(0)}
+        Else
+            Dim subPermNum As Integer = permNum / length ' (length - 1).Fact() = (!5 = !6/6)
+            Dim subPermLen As Integer
+            Dim resIndex As Integer
+
+            For i As Integer = 0 To length - 1
+                subPerm = Permutate3(n.RemoveByIndex(i), False)
+                subPermLen = subPerm(0).Length \ subPermNum
+                For j As Integer = 0 To subPermNum - 1
+                    res(resIndex) = n(i)
+                    resIndex += 1
+                    Array.Copy(subPerm(0), j * subPermLen, res, resIndex, subPermLen)
+                    resIndex += subPermLen
+                Next
+            Next
+        End If
+
+        Dim k(If(deLinearize, permNum - 1, 0))() As T
+        If deLinearize Then
+            For i As Integer = 0 To permNum - 1
+                k(i) = New T(length - 1) {}
+                Array.Copy(res, i * length, k(i), 0, length)
+            Next
+        Else
+            k(0) = New T(res.Length - 1) {}
+            Array.Copy(res, 0, k(0), 0, res.Length)
+        End If
+
+        Return k
+    End Function
+
+    <Extension()>
+    Public Iterator Function Permutate4(Of T)(n() As T) As IEnumerable(Of T())
+        Dim length As Integer = n.Length
+
+        If n.Length = 2 Then
+            Yield New T() {n(0), n(1)}
+            Yield New T() {n(1), n(0)}
+        Else
+            Dim permNum As Integer = length.Fact()
+            Dim subPermLen As Integer = length - 1
+            Dim subPermNum As Integer = permNum / length
+
+            For i As Integer = 0 To length - 1
+                For Each sp In Permutate4(n.RemoveByIndex(i))
+                    Dim r(length - 1) As T
+                    r(0) = n(i)
+                    Array.Copy(sp, 0, r, 1, subPermLen)
+                    Yield r
+                Next
+            Next
+        End If
+    End Function
+
+    <Extension>
+    Public Function Unique(Of T)(values()() As T) As T()()
+        Dim length As Integer = values(0).Length
+        Dim isEqual As Boolean = False
+        Dim res()() As T = Nothing
+        Dim resIndex As Integer
+
+        For i As Integer = 0 To values.Length - 1
+            For j As Integer = 0 To i - 1
+
+                isEqual = True
+                For k = 0 To length - 1
+                    If Not values(i)(k).Equals(values(j)(k)) Then
+                        isEqual = False
+                        Exit For
+                    End If
+                Next
+                If isEqual Then Exit For
+            Next
+
+            If Not isEqual Then
+                ReDim Preserve res(resIndex)
+                res(resIndex) = New T(length - 1) {}
+                Array.Copy(values(i), 0, res(resIndex), 0, length)
+                resIndex += 1
+            End If
+        Next
+
+        Return res
+    End Function
+
+    <Extension()>
+    Public Function Fact(value As Integer) As Double
+        Dim result As ULong = 1
+
+        For value = value To 2 Step -1
+            result *= value
+        Next
+
+        Return result
+    End Function
+
+    <Extension()>
+    Public Function ToStringList(Of T)(n() As T) As String
+        Dim r As String = ""
+        For i As Integer = 0 To n.Length - 1
+            r += n(i).ToString() + ", "
+        Next
+
+        If r <> "" Then
+            Return r.Substring(0, r.Length - 2)
+        Else
+            Return ""
+        End If
+    End Function
+End Module
--- 3DEngine/Classes/Helpers/DirectBitmap.vb
+++ 3DEngine/Classes/Helpers/DirectBitmap.vb
@@ -1,1 +1,151 @@
-../../../../TupperFormula/TupperFormula/DirectBitmap.vb
+﻿Imports System.Drawing.Imaging
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' Credit: SaxxonPike (http://stackoverflow.com/users/3117338/saxxonpike)
+' http://stackoverflow.com/questions/24701703/c-sharp-faster-alternatives-to-setpixel-And-getpixel-for-bitmaps-for-windows-f
+
+Public Class DirectBitmap
+    Implements IDisposable
+
+    Public ReadOnly Property Bitmap As Bitmap
+    Public ReadOnly Property Width As Integer
+    Public ReadOnly Property Height As Integer
+    Public ReadOnly Property Bits As Byte()
+
+    Private bitsHandle As GCHandle
+    Private w4 As Integer
+    Private bufferSize As Integer
+
+    Public Sub New(w As Integer, h As Integer)
+        Me.Width = w
+        Me.Height = h
+
+        w4 = w * 4
+        bufferSize = w4 * h - 1
+        ReDim Bits(bufferSize)
+
+        bitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned)
+        Me.Bitmap = New Bitmap(w, h, w4, PixelFormat.Format32bppPArgb,
+                               bitsHandle.AddrOfPinnedObject())
+    End Sub
+
+    Public Shared Widening Operator CType(bmp As Bitmap) As DirectBitmap
+        If bmp Is Nothing Then Return Nothing
+
+        Dim dbmp As New DirectBitmap(bmp.Width, bmp.Height)
+        Using g As Graphics = Graphics.FromImage(dbmp.Bitmap)
+            g.DrawImageUnscaled(bmp, Point.Empty)
+        End Using
+
+        Return dbmp
+    End Operator
+
+    Public Property Pixel(x As Integer, y As Integer) As Color
+        Get
+            Dim offset As Integer = y * w4 + x * 4
+            If offset < 0 OrElse offset > bufferSize Then Exit Property
+            Return Color.FromArgb(Bits(offset + 3),
+                                  Bits(offset + 2),
+                                  Bits(offset + 1),
+                                  Bits(offset + 0))
+        End Get
+        Set(value As Color)
+            Dim offset As Integer = y * w4 + x * 4
+            If offset < 0 OrElse offset > bufferSize Then Exit Property
+            Bits(offset + 3) = value.A
+            Bits(offset + 2) = value.R
+            Bits(offset + 1) = value.G
+            Bits(offset + 0) = value.B
+        End Set
+    End Property
+
+    Public Shared Narrowing Operator CType(dbmp As DirectBitmap) As Bitmap
+        If dbmp Is Nothing Then Return Nothing
+        Return dbmp.Bitmap
+    End Operator
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+                Bitmap.Dispose()
+                bitsHandle.Free()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+End Class
+
+Module DirectBitmapExtensions
+    Private Const ToRad As Double = Math.PI / 180.0
+    Private Const ToDeg As Double = 180.0 / Math.PI
+
+    <Extension>
+    Public Sub Clear(dbmp As DirectBitmap, c As Color)
+        Dim b() = {c.B, c.G, c.R, c.A}
+        Dim bufferSize As Integer = dbmp.Height * dbmp.Width * 4 - 1
+        For i As Integer = 0 To bufferSize Step 4
+            Array.Copy(b, 0, dbmp.Bits, i, 4)
+        Next
+    End Sub
+
+    <Extension()>
+    Public Sub DrawLine(dbmp As DirectBitmap, c As Color, x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        Dim dx As Integer = x2 - x1
+        Dim dy As Integer = y2 - y1
+        Dim l As Integer = Math.Sqrt(dx ^ 2 + dy ^ 2)
+        Dim a As Double = Math.Atan2(dy, dx)
+        For r As Integer = 0 To l
+            dbmp.Pixel(x1 + r * Math.Cos(-a), y1 + r * Math.Sin(a)) = c
+        Next
+    End Sub
+
+    <Extension()>
+    Public Sub DrawLine(dbmp As DirectBitmap, c As Color, p1 As Point, p2 As Point)
+        dbmp.DrawLine(c, p1.X, p1.Y, p2.X, p2.Y)
+    End Sub
+
+    <Extension()>
+    Public Sub DrawPolygon(dbmp As DirectBitmap, c As Color, p() As Point)
+        Dim j As Integer
+        Dim l As Integer = p.Length
+        For i As Integer = 0 To l - 1
+            j = (i + 1) Mod l
+            dbmp.DrawLine(c, p(i), p(j))
+        Next
+    End Sub
+
+    <Extension()>
+    Public Sub DrawPolygon(dbmp As DirectBitmap, c As Color, p() As PointF)
+        Dim l As Integer = p.Length - 1
+        Dim pi(l) As Point
+        For i As Integer = 0 To l
+            pi(i) = New Point(p(i).X, p(i).Y)
+        Next
+        dbmp.DrawPolygon(c, pi)
+    End Sub
+End Module
